5b345c6050adf44a160c983c919b3d0523d6151d
Daniel Vogel <dvogel@cs.stanford.edu>
2025-04-11 23:29:51 -0600

    rename files for clarity

--- png_generator.sh
+++ png_generator.sh
@@ -6,7 +6,7 @@ IN_PERMIT="#F1C40F"
 INACTIVE="#7F8C8D"
 
 # Define the SVG files (adjust the list as needed)
-svgs=("assets/aerodrome_green.svg" "assets/heliport_green.svg" "assets/seaplane_base_green.svg" "assets/takeoff_zone_green.svg")
+svgs=("assets/aerodrome.svg" "assets/heliport.svg" "assets/seaplane_base.svg" "assets/takeoff_zone.svg")
 
 # Loop over each file and each status version
 for svg in "${svgs[@]}"; do
@@ -14,11 +14,11 @@ for svg in "${svgs[@]}"; do
     base=$(basename "$svg" .svg)
     
     # Active version (if needed, simply copy or convert without substituting)
-    rsvg-convert -w 80 -h 80 -a -o "data/${base}_active.png" "$svg"
+    rsvg-convert -w 80 -h 80 -a -o "assets/${base}_active.png" "$svg"
     
     # In Permit version: replace the fill color with the in-permit color
-    sed "s/#00FF00/${IN_PERMIT}/Ig" "$svg" | rsvg-convert -w 80 -h 80 -a -o "data/${base}_in_permit.png"
+    sed "s/#00FF00/${IN_PERMIT}/Ig" "$svg" | rsvg-convert -w 80 -h 80 -a -o "assets/${base}_in_permit.png"
     
     # Inactive version: replace the fill color with the inactive color
-    sed "s/#00FF00/${INACTIVE}/Ig" "$svg" | rsvg-convert -w 80 -h 80 -a -o "data/${base}_inactive.png"
+    sed "s/#00FF00/${INACTIVE}/Ig" "$svg" | rsvg-convert -w 80 -h 80 -a -o "assets/${base}_inactive.png"
 done
